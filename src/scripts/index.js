a803bca516acdbd8d8981a993a4aae7c1283201b
Robert Long <longrob604@gmail.com>
2025-06-09 18:55:02 +0100

    Add header, theme toggle, particles layout, hero, &nbsp fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -74,3 +74,12 @@ document.getElementById("toggle-theme").addEventListener("click", function () {
   const newColor = isDark ? "#000000" : "#ffffff";
   updateParticleColorSmooth(newColor);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header-placeholder").innerHTML = data;
+    })
+    .catch(err => console.error("Error loading header:", err));
+});
